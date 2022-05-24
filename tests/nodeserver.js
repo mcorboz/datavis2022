1483b1cfa470eeff94f87862ab06fc6c12d2d7ee
grgvn <gregoiregavin@gmail.com>
2022-05-24 21:28:56 +0200

    tests node js

--- tests/nodeserver.js
+++ tests/nodeserver.js
@@ -1,11 +1,29 @@
-var http = require("http")
-var url = require('url')
+const http = require('http')
+const fs = require('fs')
+const express = require('express')
+const app = express()
+//const url = require("url")
 
-http.createServer(function (req, res) {
-    res.writeHead(200, { "Content-Type": "text/html" })
-    res.write("<h1>Hello World!</h1>")
-    var q = url.parse(req.url, true).query
-    var txt = "GET :" + q.year + " " + q.month // http://localhost:8000/?year=2022&month=May
-    res.write("URL :" + req.url)
-    res.end(txt)
-}).listen(8000)
+app.get('/', function (req, res) {
+
+    fs.readFile("demofile1.html", function (err, data) {
+        
+        res.writeHead(200, { "Content-Type": "text/html" })
+        res.write(data)
+
+        fs.appendFile('mynewfile1.txt', 'Hello content!', function (err) {
+            if (err) throw err
+            console.log('Saved!')
+        })
+
+        return res.end()
+        
+        /* res.write("<h1>Hello World!</h1>")
+        var q = url.parse(req.url, true).query
+        var txt = "GET :" + q.year + " " + q.month // http://localhost:8000/?year=2022&month=May
+        res.end(txt) */
+      
+    })
+})
+
+app.listen(8000)
